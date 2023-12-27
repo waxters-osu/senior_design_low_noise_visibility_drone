56a274919e88f6e16505c60de9fa3737bb92b33d
Silas Waxter <waxters@oregonstate.edu>
2023-12-27 13:50:44 -0800

    feat: make cli for working with single recording

--- audio_power_spectrum_script/main.py
+++ audio_power_spectrum_script/main.py
@@ -11,6 +11,7 @@ audio_channel_count = 1  # only care about 1 channel for this
 audio_sample_count = audio_duration_s * audio_sample_freq_hz
 audio_sample_period_s = 1.0 / audio_sample_freq_hz
 
+
 def CaptureRecording():
     print("Recording is starting...")
     recording = sd.rec(
@@ -22,37 +23,71 @@ def CaptureRecording():
     sd.wait()  # wait for recording to finish
     return recording
 
+
 def PlayRecording(recording):
     print("Audio is playing...")
     sd.play(recording, audio_sample_freq_hz)
     sd.wait()
 
+
 def EstimatePowerSpectralDensity(recording):
     # Estimate the Power Spectral Density (PSD) of the recorded audio--note that the
     # estimate is proportional to the real PSD since the microphone's sensitivity is
     # unknown.
-    recording_channel_1 = recording[:,0]
-    f, Pxx_den = signal.welch(recording_channel_1, audio_sample_freq_hz)
+    f, Pxx_den = signal.welch(recording, audio_sample_freq_hz)
     plt.semilogy(f, Pxx_den)
     plt.xlabel("frequency [Hz]")
     plt.ylabel("**proportional** PSD [W/Hz]")
     plt.show()
 
+
 def SaveRecording(recording):
     file_path = input("Enter the relative path to the recording file: ")
     sf.write(file_path, recording, audio_sample_freq_hz)
+
 
 def LoadRecording():
     file_path = input("Enter the relative path to the recording file: ")
     recording, _ = sf.read(file_path)
     return recording
 
-if __name__ == "__main__":
-    recording = CaptureRecording()
-    PlayRecording(recording)
-    EstimatePowerSpectralDensity(recording)
 
-    # Test Save and Load Recording
-    SaveRecording(recording)
-    recording = LoadRecording()
-    PlayRecording(recording)
+if __name__ == "__main__":
+    recording = None
+    while 1:
+        # prompt user for action
+        action_encoding = input(
+            "==================================================\n"
+            "Please choose one of the following actions:\n"
+            "\t[C]apture Recording\n"
+            "\t[P]lay Recording\n"
+            "\t[S]ave Recording\n"
+            "\t[L]oad Recording\n"
+            "\t[G]raph Estimate Proportional PSD of Recording\n"
+            "\t[E]xit\n"
+            "==================================================\n"
+        )
+
+        # perform action
+        if action_encoding == "C":
+            recording = CaptureRecording()
+            recording = recording[:, 0]
+        elif action_encoding == "P":
+            if recording is None:
+                print("Load or Capture Recoding First")
+            else:
+                PlayRecording(recording)
+        elif action_encoding == "S":
+            if recording is None:
+                print("Load or Capture Recoding First")
+            else:
+                SaveRecording(recording)
+        elif action_encoding == "L":
+            recording = LoadRecording()
+        elif action_encoding == "G":
+            if recording is None:
+                print("Load or Capture Recoding First")
+            else:
+                EstimatePowerSpectralDensity(recording)
+        elif action_encoding == "E":
+            exit(0)
