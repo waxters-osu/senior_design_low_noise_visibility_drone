fix(audio_spectral_script): change welch's parameters to reduce spectral leakage

audio_power_spectrum_script/main.py
@@ -6,16 +6,16 @@ import scipy.signal as signal
 
 # Audio Capture Constants
 audio_duration_s = 10  # seconds
-audio_sample_freq_hz = 44100  # std sample rate of 44100 Hz
+audio_sample_freq_hz = 44100  # 88200  # std sample rate of 44100 Hz
 audio_channel_count = 1  # only care about 1 channel for this
-audio_sample_count = audio_duration_s * audio_sample_freq_hz
+audio_sample_count = int(audio_duration_s * audio_sample_freq_hz)
 audio_sample_period_s = 1.0 / audio_sample_freq_hz
 
 
 def CaptureRecording():
     print("Recording is starting...")
     recording = sd.rec(
-        int(audio_duration_s * audio_sample_freq_hz),
+        audio_channel_count,
         samplerate=audio_sample_freq_hz,
         channels=audio_channel_count,
         # dtype="float64",
@@ -41,7 +41,15 @@ def EstimatePowerSpectralDensity(recording):
     # Estimate the Power Spectral Density (PSD) of the recorded audio--note that the
     # estimate is proportional to the real PSD since the microphone's sensitivity is
     # unknown.
-    f, Pxx_den = signal.welch(recording, audio_sample_freq_hz)
+    f, Pxx_den = signal.welch(
+        recording,
+        audio_sample_freq_hz,
+        "flattop",
+        scaling="spectrum",
+        average="median",
+        nperseg=min(audio_sample_freq_hz, audio_sample_count),
+    )
+    # f, Pxx_den = signal.periodogram(recording, audio_sample_freq_hz)
     PlotPSD(f, Pxx_den)
 
 